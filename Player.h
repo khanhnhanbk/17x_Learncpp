6df007a189a46789f56aeb02750f094cb164ddb1
unknown <nhan.nguyenbkqna@hcmut.edu.vn>
2021-04-08 21:13:31 +0700

    player.h init

--- Player.h
+++ Player.h
@@ -2,17 +2,41 @@
 #include "Creature.h"
 class Player : public Creature
 {
-private:
-    /* data */
+protected:
+    int m_level{};
+
 public:
     Player(string name);
     ~Player();
+
+    int getLevel() const;
+
+    void levelUp();
+    bool hasWon();
 };
 
-Player::Player(string name):Creature(name,'@',10,1,0)
+Player::Player(string name) : Creature(name, '@', 10, 1, 0), m_level{1}
 {
 }
 
 Player::~Player()
 {
+}
+
+int Player::getLevel() const
+{
+    return m_level;
+}
+
+void Player::levelUp()
+{
+    if (!hasWon())
+    {
+        m_level++;
+        m_damAttack++;
+    }
+}
+bool Player::hasWon()
+{
+    return m_level >= 20;
 }
--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include <string>
 #include "Creature.h"
+#include "Player.h"
+
+using namespace std;
 int main()
 {
-    Creature o{"orc", 'o', 4, 2, 10};
-    o.addGold(5);
-    o.reduceHealth(1);
-    std::cout << "The " << o.getName() << " has " << o.getHealth() << " health and is carrying " << o.getGold() << " gold.\n";
+    // Creature o{"orc", 'o', 4, 2, 10};
+    // o.addGold(5);
+    // o.reduceHealth(1);
+    // std::cout << "The " << o.getName() << " has " << o.getHealth() << " health and is carrying " << o.getGold() << " gold.\n";
+    cout << "Enter your name: ";
+    string playerName;
+    cin >> playerName;
+    Player player(playerName);
 
+    std::cout << "You has " << player.getHealth() << " health and is carrying " << player.getGold() << " gold.\n"
+              << "You in " << player.getLevel() << " level\n";
+    player.levelUp();
+    std::cout << "You has " << player.getHealth() << " health and is carrying " << player.getGold() << " gold.\n"
+              << "You in " << player.getLevel() << " level\n";
     return 0;
 }
